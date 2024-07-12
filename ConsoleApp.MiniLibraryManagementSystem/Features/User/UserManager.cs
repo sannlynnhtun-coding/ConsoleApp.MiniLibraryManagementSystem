7952cb74c39ee499f10115aed3ec5ce592cfbb46
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-13 00:19:36 +0630

    [Complete] User

---
+++ ConsoleApp.MiniLibraryManagementSystem/Features/User/UserManager.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp.MiniLibraryManagementSystem.Features.User;
+
+public class UserManager
+{
+    private List<UserDto> users = new List<UserDto>();
+
+    public void RegisterUser(string name, string email, string password)
+    {
+        users.Add(new UserDto { Id = users.Count + 1, Name = name, Email = email, Password = password });
+        Console.WriteLine($"User '{name}' registered.");
+    }
+
+    public int? AuthenticateUser(string email, string password)
+    {
+        var user = users.Find(u => u.Email == email && u.Password == password);
+        if (user != null)
+        {
+            Console.WriteLine($"User '{user.Name}' authenticated.");
+            return user.Id;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<UserDto> GetUsers()
+    {
+        return users;
+    }
+}
