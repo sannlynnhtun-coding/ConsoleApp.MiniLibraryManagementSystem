6683b1ac36195a9b7d95069f963a10f6755b9ed6
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-13 00:20:12 +0630

    [Complete] Program

--- ConsoleApp.MiniLibraryManagementSystem/Program.cs
+++ ConsoleApp.MiniLibraryManagementSystem/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp.MiniLibraryManagementSystem;
+using ConsoleApp.MiniLibraryManagementSystem.Features.Book;
+using ConsoleApp.MiniLibraryManagementSystem.Features.Report;
+using ConsoleApp.MiniLibraryManagementSystem.Features.User;
+
+Console.WriteLine("Welcome to the Library Management System!");
+
+var bookManager = new BookManager();
+var userManager = new UserManager();
+
+// Sample usage
+userManager.RegisterUser("John Doe", "john.doe@example.com", "password123");
+var userId = userManager.AuthenticateUser("john.doe@example.com", "password123");
+
+if (userId != null)
+{
+    bookManager.AddBook("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
+    bookManager.LendBook("9780743273565", userId.Value);
+    bookManager.ReturnBook("9780743273565", userId.Value);
+
+    // Pass data to the report manager
+    var reportManager = new ReportManager(bookManager.GetBooks(), bookManager.GetLendings(), userManager.GetUsers());
+
+    reportManager.GenerateBookReport();
+    reportManager.GenerateUserActivityReport();
+}
+else
+{
+    Console.WriteLine("Authentication failed.");
+}
